1779416be74433d40afcc8a094c1ed8fafa47912
lidiafc8 <lidiaestudios14@gmail.com>
2026-05-06 21:59:11 +0200

    tests frontend corregidos

--- frontend/src/matches/index.js
+++ frontend/src/matches/index.js
@@ -1,8 +1,56 @@
-export default function MatchesListing()
-{
-    return (
-        <div>
-            <h1>Matches Listing</h1>
-        </div>
-    );
+import React from "react";
+
+export default function MatchesListing() {
+  const matchesData = [
+    {
+      id: 1,
+      name: "Kasparov vs DeepBlue",
+      participants: ["Gary Kasparov", "DeepBlue"],
+    },
+    {
+      id: 2,
+      name: "The immortal",
+      participants: ["Adolf Anderssen", "Lionel Kieseritzky"],
+    },
+    {
+      id: 3,
+      name: "Game of the Century",
+      participants: ["Donald Byrne", "Bobby Fischer"],
+    },
+    {
+      id: 4,
+      name: "The Opera Game",
+      participants: ["Paul Morphy", "Duke Karl / Count Isouard"],
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Matches Listing</h1>
+      
+      <table border="1" style={{ marginTop: "20px", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th>Match Name</th>
+            <th>Participants</th>
+          </tr>
+        </thead>
+        <tbody>
+          {matchesData.map((match) => (
+            <tr key={match.id}>
+              <td>{match.name}</td>
+              
+              <td>
+                <ul style={{ margin: 0, paddingLeft: "20px" }}>
+                  {match.participants.map((participant, index) => (
+                    <li key={index}>{participant}</li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
